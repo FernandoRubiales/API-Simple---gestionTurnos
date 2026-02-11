72e1ec7c6beea2357fc44df32327255336b88653
FernandoRubiales <fernandorubiales7@gmail.com>
2026-02-10 23:30:24 -0300

    Entidades y configuraciones
    Agrego entidades del modelo, y configuro el aplication.yaml

---
+++ src/main/java/com/example/apiTurnos/entity/Turnos.java
@@ -0,0 +1,37 @@
+package com.example.apiTurnos.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Turnos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime fechaCreacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id", nullable = false)
+    private Paciente paciente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "estado_id", nullable = false)
+    private EstadoTurno estadoTruno;
+
+
+}
